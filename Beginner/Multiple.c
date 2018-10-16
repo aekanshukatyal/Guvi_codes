09584678a748c4e34ba47b2160ac34b85941a063
Aekanshu Katyal <katyalaekanshu@gmail.com>
2018-10-17 01:02:15 +0530

    Create Multiple.c

---
+++ Beginner/Multiple.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    void main()
+    {
+    int a,i,b=1,d;
+    printf("Enter no");
+    scanf("%d",&a);
+    for(i=1;i<6;i++)
+        {
+          b=a*i;  
+          printf("%d ",b);
+        }
+        return;
+
+    }
